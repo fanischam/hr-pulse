eb23256da21f2ebf0c3af179de55038aed7975d0
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-05-31 19:55:21 +0300

    enhanced data model

--- HR-Pulse/HR-Pulse/Models/Employee.cs
+++ HR-Pulse/HR-Pulse/Models/Employee.cs
@@ -6,11 +6,11 @@ namespace HR_Pulse.Models
     {
         [Key]
         public Guid Guid { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Phone { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Phone { get; set; }
         public string Title { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
